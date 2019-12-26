e2e579395a9b9cc2dd1148a0658133c3a384b2b2
Srujan Reddy <srjnreddy33@gmail.com>
2019-12-26 23:33:00 +0530

    Multiplication

---
+++ multiplication_table.py
@@ -0,0 +1,6 @@
+num = int(input("Please enter the number"))
+itr = int(input("Please enter the number of iterations that you need"))
+
+for i in range (1, itr+1):
+    print(num, ' x', i, ' =',num*i)
+    i += 1
